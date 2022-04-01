47fa91018385fa03bab7d5e9cbdd0e62513e329d
yytt77 <yytt77110@gmail.com>
2022-03-31 21:31:21 -0400

    start to work on upload.js

--- client/Upload.js
+++ client/Upload.js
@@ -1,3 +1,54 @@
-export default function() {
-  console.log('upload')
+import React, { Fragment, Component } from 'react';
+import ImagePicker from 'react-native-image-picker';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+  Image,
+  Button,
+  Dimensions,
+  TouchableOpacity
+} from 'react-native';
+
+export default function () {
+  let options = {
+    title: 'Select Image',
+    customButtons: [
+      { name: 'customOptionKey', title: 'Choose Photo from Custom Option' },
+    ],
+    storageOptions: {
+      skipBackup: true,
+      path: 'images',
+    },
+  };
+
+  /**
+* The first arg is the options object for customization (it can also be null or omitted for default options),
+* The second arg is the callback which sends object: response (more info in the API Reference)
+*/
+  ImagePicker.showImagePicker(options, (response) => {
+    console.log('Response = ', response);
+
+    if (response.didCancel) {
+      console.log('User cancelled image picker');
+    } else if (response.error) {
+      console.log('ImagePicker Error: ', response.error);
+    } else if (response.customButton) {
+      console.log('User tapped custom button: ', response.customButton);
+    } else {
+      const source = { uri: response.uri };
+
+      // You can also display the image using data:
+      // const source = { uri: 'data:image/jpeg;base64,' + response.data };
+
+      this.setState({
+        filePath: response,
+        fileData: response.data,
+        fileUri: response.uri
+      });
+    }
+  });
 }
