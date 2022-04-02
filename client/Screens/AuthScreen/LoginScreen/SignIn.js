4adedc4f44b8d0c2588355b9591205df26c02ca3
Jenya Rusinova <eugeniya.rusinova@gmail.com>
2022-04-01 20:00:51 -0700

    Added main file for Sign In

---
+++ client/Screens/AuthScreen/LoginScreen/SignIn.js
@@ -0,0 +1,11 @@
+import { Text, View } from 'react-native';
+
+export default function SignInScreen() {
+
+
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Sign in here</Text>
+    </View>
+  );
+}
