ee89169f15e661578259f1e9fd274449ba8629ff
Dominic Monares <78285106+DominicMonares@users.noreply.github.com>
2022-04-04 22:50:50 -0700

    initial registration screen

---
+++ client/Screens/AuthScreen/RegisterScreen/index.js
@@ -0,0 +1,40 @@
+import { Text, View, TouchableOpacity } from 'react-native';
+import API_IP from '../../../constants.js';
+
+import axios from 'axios';
+
+export default function RegisterScreen() {
+  const onRegisterPress = async () => {
+    console.log('TEST')
+    axios.post(`http://${API_IP}/addNewUser`, {
+      test: 1,
+      tests: 2
+    }, {
+      headers: {
+        'Content-Type': 'text/json',
+        'Accept': 'application/json'
+      }
+    })
+    .then(res => {console.log(res)})
+    .catch(err => {console.log(err)});
+    // const reg = await axios.post(`http://${API_IP}/addNewUser`, {
+    //   test: 1,
+    //   tests: 2
+    // }, {
+    //   headers: {
+    //     'Content-Type': 'text/json',
+    //     'Accept': 'application/json'
+    //   }
+    // });
+
+  // console.log('REG ', reg.data);
+}
+
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <TouchableOpacity onPress={() => onRegisterPress()}>
+        <Text>Register Screen</Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
