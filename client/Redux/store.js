b13f5f0346756b99fc4dda00824f7db21f6717a2
troyqyang <troy.q.yang@gmail.com>
2022-04-04 18:09:55 -0700

    Completed Redux store

---
+++ client/Redux/store.js
@@ -0,0 +1,14 @@
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+
+import reducers from './reducers/index';
+
+export const store = createStore(
+  reducers,
+  //default state
+  {},
+  applyMiddleware(thunk)
+);
+
+
+
