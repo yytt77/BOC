e3acfd7da5f4da51afb59d030210df212581b747
yytt77 <yytt77110@gmail.com>
2022-04-04 23:45:40 -0400

    Work on CSS for upload button

--- client/Upload.js
+++ client/Upload.js
@@ -1,17 +1,18 @@
 import React, { useState, useEffect } from 'react';
-import { StyleSheet, Button, Image, Text, View, Platform, TouchableOpacity, TextInput } from 'react-native';
+import { StyleSheet, Button, Image, Text, View, Platform, TouchableOpacity, TextInput} from 'react-native';
+import Checkbox from 'expo-checkbox';
 import * as ImagePicker from 'expo-image-picker';
 import * as Permissions from 'expo-permissions';
-// import 'font-awesome/css/font-awesome.min.css';
 import { FontAwesome } from '@expo/vector-icons';
 import * as Location from 'expo-location';
-// import { faCoffee } from "@fortawesome/free-solid-svg-icons";
+
 
 export default function Upload() {
   const [image, setImage] = useState(null);
   const [text, setText] = useState('');
   const [location, setLocation] = useState(null);
   const [errorMsg, setErrorMsg] = useState(null);
+  const [isSelected, setSelection] = useState(false);
 
   const pickImage = async () => {
 
@@ -79,27 +80,45 @@ export default function Upload() {
       latitude : location.coords.latitude,
       longitude : location.coords.longitude
     })
-    let address = await Location.geocodeAsync({
-    })
+    // let address = await Location.geocodeAsync({
+    // })
     setLocation(location);
     console.log('thisis', location);
-    console.log('address', address);
+    // console.log('address', address);
     console.log('we have city', gps[0].city, '    ', gps[0].region);
   }
 
 return (
   <View style={{ flex: 1, alignItems: 'center', backgroundColor: '#84C0FB' }}>
     <Text style={styles.header}> Post Your Pets</Text>
-
-    <TouchableOpacity style={styles.button} onPress={openCamera}>
+    <TouchableOpacity style={styles.button} onPress={pickImage}>
       {image === null ? <FontAwesome name="image" style={styles.icon} size={100} /> : <Image source={{ uri: image }} style={{ width: 300, height: 200 }} />}
     </TouchableOpacity>
-    <Text style={styles.caption}>Caption</Text>
-    <TextInput
-      style={{ height: 150, width: 300, backgroundColor: 'azure', fontSize: 15 }}
-      placeholder="Say something about your pet!"
-      onChangeText={(text) => setText({ text })}
-    />
+    <View style={styles.section}>
+      <Text style={styles.caption}>Caption </Text>
+
+      <TextInput
+        style={styles.input}
+        // style={{ height: 150, width: 300, backgroundColor: 'azure', fontSize: 15 }}
+        placeholder="Say something about your pet!"
+        onChangeText={(text) => setText({ text })}
+      />
+    </View>
+    <View style={styles.section}>
+      <Checkbox
+        value={isSelected}
+        onValueChange={setSelection}
+        style={styles.checkbox}
+        color={isSelected? '#4630EB' : undefined}
+      />
+      <Text style={styles.caption}>Share Location</Text>
+    </View>
+    <TouchableOpacity
+          style={styles.postButton}
+          // onPress={() => navigate('HomeScreen')}
+          underlayColor='#fff'>
+          <Text style={styles.post}>Post</Text>
+    </TouchableOpacity>
   </View>
 );
 }
@@ -115,7 +134,10 @@ const styles = StyleSheet.create({
     shadowRadius: 10,
     shadowOpacity: 0.35,
     width: 300,
-    height: 200
+    height: 200,
+    borderRadius:10,
+    borderColor: '#6E96BD'
+
   },
   icon: {
     color: '#D6F7D6',
@@ -127,6 +149,51 @@ const styles = StyleSheet.create({
   caption: {
     color: '#FFFFFF',
     fontSize: 15,
-    padding: 10
+    padding: 10,
+  },
+  checkbox: {
+    margin: 8,
+    backgroundColor: '#D6F7D6',
+    borderWidth: 1,
+  },
+  section: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  captionSection: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  input: {
+    height: 150,
+    width:300,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+    backgroundColor: 'azure',
+    fontSize: 15,
+    borderRadius:10,
+    borderColor: '#6E96BD'
+  },
+  post: {
+    color:'#D6F7D6',
+    textAlign:'center',
+    marginTop:5,
+    paddingLeft : 10,
+    paddingRight : 10,
+    fontSize: 20
+  },
+  postButton: {
+    height: 60,
+    width: 120,
+    marginRight:40,
+    marginLeft:40,
+    marginTop:10,
+    paddingTop:10,
+    paddingBottom:10,
+    backgroundColor:'#57D785',
+    borderRadius:10,
+    borderWidth: 1,
+    borderColor: '#6E96BD'
   }
 });
