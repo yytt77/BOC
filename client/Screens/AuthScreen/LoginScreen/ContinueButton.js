217d08e94573fc24ac6f6e2c8325a3f796c16088
Jenya Rusinova <eugeniya.rusinova@gmail.com>
2022-04-04 20:18:15 -0700

    Added several elements to logo page, logo pics to assets

--- client/Screens/AuthScreen/LoginScreen/ContinueButton.js
+++ client/Screens/AuthScreen/LoginScreen/ContinueButton.js
@@ -4,7 +4,7 @@ import { Text, View, Pressable } from 'react-native';
 export default function ContinueButton({onPress}) {
   return (
     <Pressable onPress={onPress} style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>SignIn Button</Text>
+      <Text>Continue Button</Text>
     </Pressable>
   );
 }
--- client/Screens/AuthScreen/LoginScreen/SignIn.js
+++ client/Screens/AuthScreen/LoginScreen/SignIn.js
@@ -1,8 +1,10 @@
-import { Text, View } from 'react-native';
+import { Text, View, Image } from 'react-native';
 import AccountInput from './AccountInput.js';
 import PasswordInput from './PasswordInput.js';
 import ForgotPassword from './ForgotPassword.js';
 import ContinueButton from './ContinueButton.js';
+import Logo from '../../../assets/logo_small.png';
+import BackButton from './BackButton.js';
 
 export default function SignInScreen() {
 
@@ -12,11 +14,16 @@ export default function SignInScreen() {
 
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Image
+      source = {Logo} />
+      <BackButton />
       <Text>Sign in here</Text>
       <AccountInput />
       <PasswordInput />
      <Text>Forgot password?</Text>
       <ContinueButton onPress = {onPressSignInButton} />
+      <Text>Don't have an account?</Text>
+      <Text>Sign Up</Text>
     </View>
   );
 }
