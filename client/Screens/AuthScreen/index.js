08f407e355e2cb6a0b31497f76dd06e6fbd3a9c2
Dominic Monares <78285106+DominicMonares@users.noreply.github.com>
2022-04-04 22:52:35 -0700

    temp change to rendering for Registration dev

--- client/Screens/AuthScreen/index.js
+++ client/Screens/AuthScreen/index.js
@@ -1,5 +1,5 @@
 import { Text, View } from 'react-native';
-import RegisterScreen from './RegisterScreen'
+import RegisterScreen from './RegisterScreen' //temp
 
     // <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
     //   <Text>Random Feed Here</Text>
