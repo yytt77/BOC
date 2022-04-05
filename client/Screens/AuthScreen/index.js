9d9f85649665ecd57864547dab2469365173bf1e
Dominic Monares <78285106+DominicMonares@users.noreply.github.com>
2022-04-04 22:50:26 -0700

    temporarily change rendering for Registration dev

--- client/Screens/AuthScreen/index.js
+++ client/Screens/AuthScreen/index.js
@@ -1,10 +1,12 @@
 import { Text, View } from 'react-native';
+import RegisterScreen from './RegisterScreen'
 
+    // <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+    //   <Text>Random Feed Here</Text>
+    //   <Text>Login Button</Text>
+    // </View>
 export default function AuthScreen() {
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Random Feed Here</Text>
-      <Text>Login Button</Text>
-    </View>
+    <RegisterScreen />
   );
 }
