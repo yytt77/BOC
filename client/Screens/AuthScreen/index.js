033e3bb49188c737420ffdb566afbb6d53194934
Dominic Monares <78285106+DominicMonares@users.noreply.github.com>
2022-04-06 16:02:38 -0700

    render Authenticate

--- client/Screens/AuthScreen/index.js
+++ client/Screens/AuthScreen/index.js
@@ -1,12 +1,11 @@
 import { Text, View } from 'react-native';
 import Authenticate from './AuthScreen';
 
-//<View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-//  <Text>Random Feed Here</Text>
-//  <Text>Login Button</Text>
-//</View>
 export default function AuthScreen() {
   return (
-    <Authenticate />
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Random Feed Here</Text>
+      <Authenticate />
+    </View>
   );
 }
