f331459352b1ba1e26859519d558a68fbc7b3893
Dominic Monares <78285106+DominicMonares@users.noreply.github.com>
2022-04-06 22:55:47 -0700

    implement userAuthReducer

--- client/Redux/reducers/index.js
+++ client/Redux/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import uploadProfilePictureReducer from './uploadProfilePictureReducer';
+import userAuthReducer from './userAuthReducer';
 
 const reducers = combineReducers({
-  uploadProfilePictureClickCount: uploadProfilePictureReducer
+  uploadProfilePictureClickCount: uploadProfilePictureReducer,
+  user: userAuthReducer
 });
 
 export default reducers;
