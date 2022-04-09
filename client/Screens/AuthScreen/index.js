0c8a0778dcaf7db2eab56770643aa93ec99f09e8
Dominic Monares <78285106+DominicMonares@users.noreply.github.com>
2022-04-09 12:59:28 -0700

    separate signin and register

--- client/Screens/AuthScreen/index.js
+++ client/Screens/AuthScreen/index.js
@@ -1,11 +1,18 @@
+import { useState, getState } from 'react';
 import { Text, View } from 'react-native';
+
 import Authenticate from './AuthScreen';
+import SignInScreen from './LoginScreen/SignIn';
+import RegisterScreen from './RegisterScreen/Register';
 
 export default function AuthScreen() {
+  const [screen, setScreen] = useState('login');
+
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Random Feed Here</Text>
-      <Authenticate />
-    </View>
+    screen === 'login' ? (
+      <SignInScreen />
+    ) : (
+      <RegisterScreen />
+    )
   );
 }
