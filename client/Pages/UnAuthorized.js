7ae2ff346dc4a7a974cc07eb5003efe7ddfa0d7d
Dominic Monares <78285106+DominicMonares@users.noreply.github.com>
2022-04-09 21:35:39 -0700

    temporarily make auth the default view

--- client/Pages/UnAuthorized.js
+++ client/Pages/UnAuthorized.js
@@ -4,11 +4,10 @@ import { Button, Text, View } from "react-native";
 import AuthScreen from "../Screens/AuthScreen/index";
 
 const UnAuthorized = () => {
-  const [screen, setScreen] = useState('home')
+  // home will be default once implemented
+  const [screen, setScreen] = useState('auth')
 
   return (
-    // Set home to auth to view home screen after login pressed
-    // until back button is implemented
     screen === 'home' ? (
       <View style={{flex: 1, justifyContent: 'center'}}>
         <Text>Discovery Feed Here</Text>
