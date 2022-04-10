3e98613ee9db1160e46d620dd50125acff826bce
Dominic Monares <78285106+DominicMonares@users.noreply.github.com>
2022-04-09 21:05:45 -0700

    add screen state and conditional render

--- client/Pages/UnAuthorized.js
+++ client/Pages/UnAuthorized.js
@@ -1,8 +1,21 @@
-import { View, Text } from "react-native";
-import AuthScreen from "../Screens/AuthScreen";
+import { useState } from 'react';
+import { Button, Text, View } from "react-native";
+
+import AuthScreen from "../Screens/AuthScreen/index";
 
 const UnAuthorized = () => {
-  return <AuthScreen/>;
+  const [screen, setScreen] = useState('home')
+
+  return (
+    screen === 'auth' ? (
+      <View style={{flex: 1, justifyContent: 'center'}}>
+        <Text>Discovery Feed Here</Text>
+        <Button title={'Login Icon Here'} onPress={() => setScreen('auth')}></Button>
+      </View>
+    ) : (
+      <AuthScreen />
+    )
+  )
 };
 
 export default UnAuthorized;
