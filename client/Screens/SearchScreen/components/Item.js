f94b5a23e85f6c87766cd8bd591b457fc0074185
Joseph Nahm <josephnahm1@gmail.com>
2022-04-12 00:33:36 -0700

    implemented chat. Need styling

--- client/Screens/SearchScreen/components/Item.js
+++ client/Screens/SearchScreen/components/Item.js
@@ -3,6 +3,7 @@ import { getLocally, storeLocally } from "../../../LocalStorage";
 import { Item as styles } from "../Styles";
 import { API_IP } from "../../../constants";
 import axios from "axios";
+import { FontAwesome5, FontAwesome } from "@expo/vector-icons";
 const emptyHistory = "No Recent Searches";
 const recentHistory = "Recent Searches";
 const emptyQuery = "User not found";
@@ -21,8 +22,11 @@ export default (
   { item },
   deleteHistory,
   openOtherUser,
+  openChat,
   recentSearches,
-  setRecentSearches
+  setRecentSearches,
+  textColor,
+  searchIconColor
 ) => {
   const onUserPress = async (username) => {
     try {
@@ -50,29 +54,64 @@ export default (
     }
   };
 
+  const chatUser = async (username) => {
+    try {
+      const user = await axios.get(`${userEndpoint}${item.username}`);
+      openChat(user.data)
+    } catch (err) {
+      console.error(err);
+      alert("Not Able to Chat User");
+    }
+  };
+
   if (item === recentHistory)
     return (
       <View style={styles.historyLabel}>
-        <Text style={styles.comicText}>{item}</Text>
+        <Text style={[styles.comicText, { color: textColor }]}>{item}</Text>
         <TouchableOpacity onPress={deleteHistory}>
           <Text style={styles.clear}>Clear All</Text>
         </TouchableOpacity>
       </View>
     );
   if (item === emptyHistory || item === emptyQuery || item === failedConnection)
-    return <Text style={styles.historyLabel}>{item}</Text>;
+    return (
+      <Text style={[styles.historyLabel, { color: textColor }]}>{item}</Text>
+    );
   return (
     <TouchableOpacity onPress={() => onUserPress(item.username)}>
       <View>
         <View style={styles.item}>
-          {item.profPhoto ? <Image
-            style={styles.profileImage}
-            resizeMode="cover"
-            source={{
-              uri: `${item.profPhoto}`,
-            }}
-          />: <Text>Placeholder Photo</Text>}
-          <Text style={styles.comicText}>{item.username}</Text>
+          {item.profPhoto ? (
+            <Image
+              style={styles.profileImage}
+              resizeMode="cover"
+              source={{
+                uri: `${item.profPhoto}`,
+              }}
+            />
+          ) : (
+            <View
+              style={[styles.noProfileImage, { backgroundColor: searchIconColor }]}
+            >
+              <FontAwesome5
+                name="user-alt"
+                size={20}
+                color="grey"
+              ></FontAwesome5>
+            </View>
+          )}
+          <View style={styles.right}>
+            <TouchableOpacity onPress={() => chatUser(item.username)}>
+              <View style={[styles.noProfileImage, { backgroundColor: searchIconColor }]}>
+                <FontAwesome name="send-o" size={24} color={textColor} />
+              </View>
+            </TouchableOpacity>
+            <Text
+              style={[styles.comicText, styles.username, { color: textColor }]}
+            >
+              {item.username}
+            </Text>
+          </View>
         </View>
       </View>
     </TouchableOpacity>
