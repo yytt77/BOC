14c66b6f4528a70b47ba69a29cb8a328f99cd038
troyqyang <troy.q.yang@gmail.com>
2022-04-13 18:14:59 -0700

    Logout functionality and user upload profile photo functionality.

--- client/Redux/reducers/index.js
+++ client/Redux/reducers/index.js
@@ -1,6 +1,5 @@
 import { combineReducers } from 'redux';
 import userReducer from './user';
-import uploadProfilePictureReducer from './uploadProfilePictureReducer';
 import updateColorSchemeReducer from './updateColorSchemeReducer';
 import authScreenReducer from './authScreenReducer';
 import guestHomeReducer from './guestHomeReducer';
