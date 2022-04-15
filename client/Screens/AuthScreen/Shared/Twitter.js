0805d926f9e5f32b38eefaa24190530ce6c6a21f
Dominic Monares <78285106+DominicMonares@users.noreply.github.com>
2022-04-15 00:55:38 -0700

    add Linking

--- client/Screens/AuthScreen/Shared/Twitter.js
+++ client/Screens/AuthScreen/Shared/Twitter.js
@@ -1,4 +1,5 @@
-import { Button, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Button, Linking, Text, View } from 'react-native';
 import axios from 'axios';
 
 import { API_IP } from '../../../constants.js';
@@ -6,19 +7,27 @@ import { API_IP } from '../../../constants.js';
 const twitterRegEndpoint = `http://${API_IP}/user/auth/twitter`;
 
 export default function Twitter() {
+  const [test, setTest] = useState(<></>);
+
   const twitterReg = async () => {
-    try {
-      const register = await axios.get(twitterRegEndpoint);
-      console.log('T ', register.data);
-    } catch (err) {
-      console.log(err);
-    }
+    // try {
+    //   const register = await axios.get(twitterRegEndpoint);
+    //   console.log('T ', register.data);
+    //   await Linking.openURL(register.data);
+    // } catch (err) {
+    //   console.log(err);
+    // }
+
+    Linking.openURL(twitterRegEndpoint);
   }
 
   return (
     // button will include Twitter logo
     <View>
       <Button title="Register with Twitter" onPress={ () => twitterReg() }>Register with Twitter</Button>
+      <Text>
+        {test}
+      </Text>
     </View>
   )
 }
