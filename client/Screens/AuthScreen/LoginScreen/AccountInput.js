7ae670a446facdea42ba762a84e9941fd16b5826
Jenya Rusinova <eugeniya.rusinova@gmail.com>
2022-04-15 22:16:34 -0700

    Connecting login with backend - work in progress

--- client/Screens/AuthScreen/LoginScreen/AccountInput.js
+++ client/Screens/AuthScreen/LoginScreen/AccountInput.js
@@ -5,18 +5,39 @@ import axios from 'axios';
 
 
 import { Register as styles } from '../RegisterScreen/Styles'
-//import { API_IP } from '../../../constants.js';
-//import { login } from '../../../Redux/actions';
+import { API_IP } from '../../../constants.js';
+import { login } from '../../../Redux/actions';
 
-//const loginEndpoint = `http://${API_IP}/user//login/password`;
+const loginEndpoint = `http://${API_IP}/user/login/password`;
 
 export default function AccountInput(){
+  const state = useSelector(state => state);
+  const dispatch = useDispatch();
+  const screen = useSelector(state => state.authScreen);
 
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
 
   const handleLogin = async () => {
-     console.log('handleLogin was called');
-
+     console.log('endpoint', loginEndpoint);
+     console.log('username', username);
+     console.log('password', password);
   }
+
+  axios.post(loginEndpoint, {
+    username: username,
+    password: password
+  })
+  .then(function (response) {
+    dispatch(login())
+    console.log('successfully sent login data to backend');
+  })
+  .catch(function (error) {
+    console.log('error sending login', error);
+    console.log(error.response.data)
+  });
+
+
 
   const handleForgotPassword = async () => {
     console.log('handleForgotPassword was called');
@@ -31,11 +52,15 @@ export default function AccountInput(){
     <Text>Account name</Text>
     <TextInput
       style={styles.field}
+      onChangeText={text => setUsername(text)}
+
     />
     <Text>Password</Text>
     <TextInput
       style={styles.field}
       secureTextEntry={true}
+      onChangeText={text => setPassword(text)}
+
     />
     <Button title={'ForgotPassword'} onPress={() => handleForgotPassword()}>Forgot password?</Button>
 
