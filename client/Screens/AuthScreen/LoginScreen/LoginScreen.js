b8aabc88d44ddc1e3a78af6a3dc8d4ea70ac7054
Jenya Rusinova <eugeniya.rusinova@gmail.com>
2022-04-15 20:24:03 -0700

    Refactored login screen

--- client/Screens/AuthScreen/LoginScreen/LoginScreen.js
+++ client/Screens/AuthScreen/LoginScreen/LoginScreen.js
@@ -1,12 +1,30 @@
+//import { Text, View, Image } from 'react-native';
+
 import { useState } from 'react';
 import { Button, Text, TextInput, View } from 'react-native';
 import { connect } from 'react-redux';
 
-import SignInInput from './AccountInput';
-import { LoginScreen as styles } from '../RegisterScreen/Styles'
+import PetPixLogo from '../../../Templates/PetPixLogo';
+import Tabs from '../Shared/Tabs';
+import BackButton from '../Shared/BackButton';
+import Google from '../Shared/Google';
+import Twitter from '../Shared/Twitter';
+
+
+import AccountInput from './AccountInput.js';
+import PasswordInput from './PasswordInput.js';
+import ForgotPassword from './ForgotPassword.js';
+import ContinueButton from './ContinueButton.js';
+import Logo from '../../../assets/logo_small.png';
+
+
+//import { Register as styles } from '..RegisterScreen/Styles'
+import { Register as styles } from '../RegisterScreen/Styles'
+
 import { lightTheme, darkTheme } from '../../../constants';
 
-const LoginScreen = function(props) {
+
+const SignInScreen = function(props){
   var theme;
   if (props.theme) {
     theme = lightTheme;
@@ -14,12 +32,16 @@ const LoginScreen = function(props) {
     theme = darkTheme;
   }
 
+
   return (
     <View style={[{ backgroundColor: theme.pageColor }, styles.container]}>
-      <View>
-        <Text>Login container here</Text>
-      </View>
-      <SignInInput />
+      <PetPixLogo />
+      <BackButton />
+      <Tabs />
+      <AccountInput />
+      <PasswordInput />
+      <Text>Forgot password?</Text>
+      <ContinueButton />
     </View>
   )
 }
@@ -28,4 +50,26 @@ const mapStateToProps = (state) => ({
   theme: state.theme
 })
 
-export default connect(mapStateToProps, null)(LoginScreen);
+export default connect(mapStateToProps, null)(SignInScreen);
+
+
+// export default function SignInScreen() {
+
+//    const onPressSignInButton = () => {
+//      //send login and password to backend
+//      //receive answer from backend
+//    }
+
+//   return (
+//     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+//       <Image
+//       source = {Logo} />
+//       <BackButton />
+//       <Text>Sign in here</Text>
+//       <AccountInput />
+//       <PasswordInput />
+//      <Text>Forgot password?</Text>
+//       <ContinueButton onPress = {onPressSignInButton} />
+//     </View>
+//   );
+// }//
