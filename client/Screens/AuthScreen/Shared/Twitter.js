df74b3cebf5ce718f30978773dee2655d8af3a46
Dominic Monares <78285106+DominicMonares@users.noreply.github.com>
2022-04-16 00:51:52 -0700

    add dispatch

--- client/Screens/AuthScreen/Shared/Twitter.js
+++ client/Screens/AuthScreen/Shared/Twitter.js
@@ -1,54 +1,51 @@
-import { useState } from 'react';
-import { Alert, Button, Linking, Text, View } from 'react-native';
+import { useCallback, useState } from 'react';
+import { Alert, Button, Text, View } from 'react-native';
+import { useSelector, useDispatch } from 'react-redux';
+import * as Linking from 'expo-linking';
 import * as WebBrowser from 'expo-web-browser';
-import * as AuthSession from 'expo-auth-session';
 
 import axios from 'axios';
 
+import { login } from '../../../Redux/actions';
 import { API_IP } from '../../../constants.js';
 const twitterRegEndpoint = `http://${API_IP}/user/auth/twitter`;
 const twitterCbEndpoint = `http://${API_IP}/user/auth/twitter/callback`;
 
+WebBrowser.maybeCompleteAuthSession();
+
 export default function Twitter() {
-  const [webAuth, setWebAuth] = useState(<></>);
+  const state = useSelector(state => state);
+  const dispatch = useDispatch();
+  const screen = useSelector(state => state.user);
 
-  const twitterReg = async () => {
+  const _handleRedirect = event => {
+    WebBrowser.dismissBrowser();
+    let data = Linking.parse(event.url);
+    console.log('DATA ', data);
+    setUser(data);
+  }
 
-    // console.log('RES ', results);
+  const _openAuthSessionAsync = async () => {
+    try {
+      let result = await WebBrowser.openAuthSessionAsync(twitterCbEndpoint);
+      let redirectData;
+      if (result.url) {
+        redirectData = Linking.parse(result.url);
+      }
 
-    // Linking.addEventListener('url', (url) => {
-    //   console.log('URL ', url)
-
-    //   WebBrowser.dismissBrowser();
-    // })
-
-    let result = await WebBrowser.openBrowserAsync(twitterRegEndpoint);
-    console.log('RES ', result)
-    setWebAuth(result);
-
-
-    // Linking.openURL(twitterRegEndpoint);
-
-    // try {
-    //   const register = await axios.get(twitterRegEndpoint);
-    //   console.log('T ', register.data);
-    //   await Linking.openURL(register.data);
-    // } catch (err) {
-    //   console.log(err);
-    // }
+      let username = redirectData.queryParams.username;
+      // let email = redirectData.queryParams.email;
+      dispatch(login(username));
+    } catch (err) {
+      alert(err);
+      console.log(err);
+    }
   }
 
   return (
     // button will include Twitter logo
     <View>
-      <Button title="Register with Twitter" onPress={ () => twitterReg() }>Register with Twitter</Button>
-      <Text>
-        {typeof webAuth !== 'object' ? (
-          webAuth
-        ) : (
-          <></>
-        )}
-      </Text>
+      <Button title="Register with Twitter" onPress={ () => _openAuthSessionAsync() }>Register with Twitter</Button>
     </View>
   )
 }
