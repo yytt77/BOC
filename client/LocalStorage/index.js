5caf6eaa56e7b495ce97b462c005a73edb2ea5eb
Dominic Monares <78285106+DominicMonares@users.noreply.github.com>
2022-04-16 17:36:45 -0700

    temporarily replace AsyncStorage for testing

--- client/LocalStorage/index.js
+++ client/LocalStorage/index.js
@@ -1,4 +1,5 @@
-import AsyncStorage from '@react-native-async-storage/async-storage';
+// import AsyncStorage from '@react-native-async-storage/async-storage';
+import { AsyncStorage } from 'react-native';
 
 export const storeLocally = async (key, value) => {
   try {
