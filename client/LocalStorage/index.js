7745e1b8663a81698c321e7970017c1c52d30a69
Joseph Nahm <josephnahm1@gmail.com>
2022-04-18 18:47:03 -0700

    need to test chat functionality but interacts with deployed server

--- client/LocalStorage/index.js
+++ client/LocalStorage/index.js
@@ -1,7 +1,7 @@
-// import AsyncStorage from '@react-native-async-storage/async-storage';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 // Only used for testing, revert for production
-import { AsyncStorage } from 'react-native';
+// import { AsyncStorage } from 'react-native';
 
 export const storeLocally = async (key, value) => {
   try {
