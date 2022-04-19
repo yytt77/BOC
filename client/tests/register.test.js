eed2da9f7404e09c5b9a4915c95e35689291a7b7
Dominic Monares <78285106+DominicMonares@users.noreply.github.com>
2022-04-18 23:22:30 -0700

    save progress

--- client/tests/register.test.js
+++ client/tests/register.test.js
@@ -2,28 +2,46 @@ import React from 'react';
 import renderer from 'react-test-renderer';
 
 import AppWrapper from '../App';
+import Register from '../Screens/AuthScreen/RegisterScreen/Register';
 
-describe('App', () => {
+describe('Register', () => {
 
-  const tree = renderer.create(<AppWrapper />).toJSON();
   const c = 'children';
 
-  it('should render AppWrapper', () => {
+  xdescribe('UnAuthorized', () => {
 
-    // AppWrapper renders UnAuthorized page by default
-    expect(tree.children.length).toBe(2);
+    const tree = renderer.create(<AppWrapper />).toJSON();
+
+    it('should render AppWrapper', () => {
+      // AppWrapper renders UnAuthorized page by default
+      expect(tree.children.length).toBe(2);
+    })
+
+    it('should render guest discovery feed background', () => {
+      // create additional tests/change this one to be more meaningful once
+      // guest discovery feed is complete
+      const feedBGC = tree[c][0]['props']['style'][1]['backgroundColor'];
+      expect(feedBGC).toBe('#84C0FB');
+    })
+
+    // Snapshot Test
+    it('should render app wrapper', () => {
+      expect(tree).toMatchSnapshot();
+    })
+
   })
 
-  it('should render guest discovery feed background', () => {
-    // create additional tests/change this one to be more meaningful once
-    // guest discovery feed is complete
-    const feedBGC = tree[c][0]['props']['style'][1]['backgroundColor'];
-    expect(feedBGC).toBe('#84C0FB');
+  describe('Registration', () => {
+
+    const tree = renderer.create(<Register />).toJSON();
+
+    it('should render user, email, and two password fields', () => {
+      console.log('TREE ', tree)
+      expect(tree.children.length).toBe(2);
+    })
+
   })
 
-  // Snapshot Test
-  it('should render app wrapper', () => {
-    expect(tree).toMatchSnapshot();
-  })
+
 
 })
