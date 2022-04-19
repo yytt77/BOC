da8b002076fbe1df1a8d68b787163bad1bdc9e01
Dominic Monares <78285106+DominicMonares@users.noreply.github.com>
2022-04-18 17:56:40 -0700

    relocate register

---
+++ client/tests/register.test.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+
+import AppWrapper from '../../App';
+
+describe('App', () => {
+
+  // Unit Test
+  it('should render AppWrapper', () => {
+    const tree = renderer.create(<AppWrapper />).toJSON();
+
+    // AppWrapper renders UnAuthorized page by default
+    expect(tree.children.length).toBe(2);
+  })
+
+  // Unit Test
+  it('should render guest discovery feed', () => {
+    const tree = renderer.create(<AppWrapper />).toJSON();
+    const feed = tree['children'][0]['children'][0];
+
+    // placeholder text from UnAuthorized page
+    expect(feed).toBe('Discovery Feed Here');
+  })
+
+  // Snapshot Test
+  it('should render app wrapper', () => {
+    const tree = renderer.create(<AppWrapper />).toJSON();
+    expect(tree).toMatchSnapshot();
+  })
+
+})
