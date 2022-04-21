256d44b97c2affee0a92b1f41f612abc5b1d5c45
Dominic Monares <78285106+DominicMonares@users.noreply.github.com>
2022-04-20 23:27:06 -0700

    change DiscoverScreen render, organize imports

--- client/Pages/UnAuthorized.js
+++ client/Pages/UnAuthorized.js
@@ -1,19 +1,34 @@
-import { useState, getState } from 'react';
-import { Button, Text, View } from "react-native";
-import { useSelector, useDispatch } from 'react-redux';
+// React | React-Native
+import { Button, View } from "react-native";
+// import { NavigationContainer, StackRouter } from '@react-navigation/native';
+// import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
-import DiscoverScreen from '../Screens/DiscoverScreen/index';
-import AuthScreen from '../Screens/AuthScreen/index';
+// Redux
+import { useSelector, useDispatch } from 'react-redux';
 import { guestAuth } from '../Redux/actions';
 
-const UnAuthorized = () => {
+// Components
+import Header from '../Screens/AuthScreen/Shared/Header';
+import * as Logo from '../Templates/HeaderTemplate';
+import DiscoverScreen from '../Screens/DiscoverScreen/index';
+import AuthScreen from '../Screens/AuthScreen/index';
+
+
+// Styling
+import styles from "./appStyles";
+import { palette } from '../Utils/ColorScheme';
+import { FontAwesome5, Entypo } from "@expo/vector-icons";
+
+
+export default function UnAuthorized() {
+  const state = useSelector(state => state);
   const guestHome = useSelector(state => state.guestHome);
   const dispatch = useDispatch();
 
-  // <DiscoverScreen />
   return (
     guestHome === 'home' ? (
-      <View style={{flex: 1, justifyContent: 'center'}}>
+      <View style={{ flex: 1, justifyContent: 'center' }}>
+        <DiscoverScreen />
         <Button title={'Login Icon Here'} onPress={() => dispatch(guestAuth())}></Button>
       </View>
     ) : (
@@ -21,5 +36,3 @@ const UnAuthorized = () => {
     )
   )
 };
-
-export default UnAuthorized;
