01e7e4b97465980fa047907e300c683dc57db22f
Joseph Nahm <josephnahm1@gmail.com>
2022-04-20 20:29:09 -0700

    group code review

--- client/Pages/index.js
+++ client/Pages/index.js
@@ -9,15 +9,15 @@ const App = function() {
   const state = useSelector(state => state);
   const user = useSelector((state) => state.user);
 
-  console.log('PROPS ', state)
-  // return (
-  //   state.user.username ? (
-  //     <Authorized />
-  //   ) : (
-  //     <UnAuthorized />
-  //   )
-  // )
-  return <Authorized />
+  // console.log('PROPS ', state)
+  return (
+    state.user.username ? (
+      <Authorized />
+    ) : (
+      <UnAuthorized />
+    )
+  )
+  // return <Authorized />
 }
 
 // const mapStateToProps = (state) => ({
