2e8a058920203280ab5ff267b877a69e778f0d37
yytt77 <yytt77110@gmail.com>
2022-04-20 23:39:06 -0400

    Delete unnecessary test file

--- client/tests/sample.test.js
+++ client/tests/sample.test.js
@@ -6,21 +6,21 @@ import AppWrapper from '../App';
 describe('App', () => {
 
   // Unit Test
-  // it('should render AppWrapper', () => {
-  //   const tree = renderer.create(<AppWrapper />).toJSON();
+  it('should render AppWrapper', () => {
+    const tree = renderer.create(<AppWrapper />).toJSON();
 
-  //   // AppWrapper renders UnAuthorized page by default
-  //   expect(tree.children.length).toBe(2);
-  // })
+    // AppWrapper renders UnAuthorized page by default
+    expect(tree.children.length).toBe(2);
+  })
 
   // Unit Test
-  // it('should render guest discovery feed', () => {
-  //   const tree = renderer.create(<AppWrapper />).toJSON();
-  //   const feed = tree['children'][0]['children'][0];
+  it('should render guest discovery feed', () => {
+    const tree = renderer.create(<AppWrapper />).toJSON();
+    const feed = tree['children'][0]['children'][0];
 
-  //   // placeholder text from UnAuthorized page
-  //   expect(feed).toBe('Discovery Feed Here');
-  // })
+    // placeholder text from UnAuthorized page
+    expect(feed).toBe('Discovery Feed Here');
+  })
 
   // Snapshot Test
   it('should render app wrapper', () => {
