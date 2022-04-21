b94d99d7753a193c1a304d2d3a9b97ddbe58584a
Dominic Monares <78285106+DominicMonares@users.noreply.github.com>
2022-04-20 23:39:12 -0700

    implement header

---
+++ client/Screens/AuthScreen/Shared/Header.js
@@ -0,0 +1,18 @@
+import { View } from "react-native";
+import { useSelector } from 'react-redux';
+import { lightTheme, darkTheme } from '../../../constants';
+
+export default function Header() {
+  const theme = useSelector(state => state.theme);
+  let current;
+
+  if (theme) {
+    current = lightTheme;
+  } else {
+    current = darkTheme;
+  }
+
+  return (
+    <View style={[{ backgroundColor: current.navColor, height: 50 }]} />
+  )
+}
