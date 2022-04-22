a7971a2aba1d9be938efd807a618cb993c12cf52
Jenya Rusinova <eugeniya.rusinova@gmail.com>
2022-04-22 11:01:44 -0700

    Added tests for login screen

--- client/tests/login.test.js
+++ client/tests/login.test.js
@@ -32,10 +32,55 @@ describe('Login', () => {
 
       const tree = Login.toJSON();
       expect(tree).toMatchSnapshot();
+      expect(tree.children.length).toBe(6);
+      expect(tree.props.style.length).toBe(2);
+      expect(tree.props.style[1]).toEqual( {"flex": 1, "justifyContent": "center"});
+      expect(tree.props.style[0].backgroundColor).toEqual('#84C0FB');
     })
 
   })
 
- 
+  describe('Account Input', () => {
+    let store, acctInput;
+
+    beforeEach(() => {
+      store = mockStore(state);
+      acctInput = <Provider store={store}>
+        <AccountInput />
+      </Provider>
+    })
+
+    it('should render Account Input', () => {
+      const Account = create(
+        <Provider store={store}>
+          <AccountInput />
+        </Provider>
+      );
+
+      const tree = Account.toJSON();
+      expect(tree.children.length).toBe(8);
+      expect(tree.props.style).toEqual({width: '70%'});
+      expect(tree.children[0].children).toEqual(["Account name"]);
+      expect(tree.children[2].children).toEqual(["Password"]);
+
+  })
+
+  xit('should correctly change login and password', () => {
+    const Account = create(
+      <Provider store={store}>
+        <AccountInput />
+      </Provider>
+    );
+
+    const { getByText, toJSON } = render(acctInput);
+    fireEvent.press(getByText('SignUp'));
+    const tree = Account.toJSON();
+    //console.log(tree);
+
+
+})
+
+
+})
 
 })
