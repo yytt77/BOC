2344ec2e2678dc78959c0b788d1254744525dd68
Joseph Nahm <josephnahm1@gmail.com>
2022-04-22 17:04:07 -0700

    fixed font load async problem

--- client/App.js
+++ client/App.js
@@ -1,10 +1,19 @@
 import { View } from "react-native";
 import { Provider } from 'react-redux';
+import { useFonts } from "expo-font";
+import AppLoading from 'expo-app-loading';
 
 import { store } from './Redux/store';
 import App from "./Pages/index";
 import ScreenCapture from "./Utils/ScreenShotDetector"
 export default function AppWrapper() {
+  const [fontsLoaded] = useFonts({
+    comicSans: require('./assets/fonts/comic.ttf')
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
   return(
     <Provider store={store}>
       <App />
